70b5706d2b77e24a5673fea134305da67315dc4d
J.S.LI <j.s.li@linux.com>
2016-08-02 11:49:31 +0800

    The revision which literally finish all the function needed, except for dynamically allocate the options in QComboBox. Also, the Language options need to be refined.

--- src/ManjaroWindowsInstaller/manjarowindowsinstaller.cpp
+++ src/ManjaroWindowsInstaller/manjarowindowsinstaller.cpp
@@ -3,6 +3,7 @@
 #include "iostream"
 #include "fstream"
 #include "qstring.h"
+#include "windows.h"
 
 ManjaroWindowsInstaller::ManjaroWindowsInstaller(QWidget *parent) :
     QMainWindow(parent),
@@ -40,18 +41,22 @@ void ManjaroWindowsInstaller::on_pushButton_clicked()
 void ManjaroWindowsInstaller::proceedInstallation()
 {
     //Export config
+        ui->progressBar->setValue(0);
         //Q-styled string
         QString Qlanguage = ui->comboBox->currentText();
         QString Qdrive = ui->comboBox_2->currentText();
         QString QinstallationSize = ui->plainTextEdit_3->toPlainText();
         QString Qusername = ui->plainTextEdit->toPlainText();
         QString Qpassword = ui->plainTextEdit_2->toPlainText();
+        ui->progressBar->setValue(20);
+
         //Convert to standard string
         std::string language = Qlanguage.toUtf8().constData();
         std::string drive = Qdrive.toUtf8().constData();
         std::string installationSize = QinstallationSize.toUtf8().constData();
         std::string username = Qusername.toUtf8().constData();
         std::string password = Qpassword.toUtf8().constData();
+        ui->progressBar->setValue(40);
 
         std::ofstream configFile;
         configFile.open("manjaro.config");
@@ -63,12 +68,17 @@ void ManjaroWindowsInstaller::proceedInstallation()
         configFile.close();
         system("mkdir %SYSTEMDRIVE%\\manjaro");
         system("copy manjaro.config %SYSTEMDRIVE%\\manjaro\\manjaro.config");
+        ui->progressBar->setValue(50);
 
     //Make Manjaro ISO bootable.
         //copy grub4dos folder and manjaro ISO to C:\manjaro
-        system("copy manjaro.iso %SYSTEMDRIVE%\\manjaro\\manjaro.iso");
+        system("move manjaro.iso %SYSTEMDRIVE%\\manjaro\\manjaro.iso");
         system("xcopy grub4dos %SYSTEMDRIVE%\\grub4dos /s /e");
+        ui->progressBar->setValue(75);
 
-        //edit boot.ini to boot grldr file (TBD)
+        //edit boot.ini to boot grldr file (TBD) add C:\manjaro\grub4dos\grldr="Manjaro Linux"
+        std::ofstream changeBootINI("%SYSTEMDRIVE%\\boot.ini", std::ios_base::app | std::ios_base::out);
+        changeBootINI << "\n" << "%SYSTEMDRIVE%\\manjaro\\grub4dos\\grldr=\"ManjaroLinux\""<< std::endl;
+        changeBootINI.close();
 
 }
